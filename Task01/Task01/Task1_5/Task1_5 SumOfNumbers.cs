7c9c4b1bcbb0ede75ab1f67a50f6ef19e9c7ef96
AlexeyZabuga <arquenz@yandex.ru>
2019-11-24 13:13:36 +0400

    Добавил Task 1_5

---
+++ Task01/Task01/Task1_5/Task1_5 SumOfNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_5
+{
+    class Task1_5
+    {
+        static void Main(string[] args)
+        {
+            SumOfNumbers();
+            Console.ReadLine();
+        }
+        private static void SumOfNumbers()
+        {
+            int summ = 0;
+            int i = 3;
+            while (i < 1000)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    summ += i;
+                }
+                i++;
+            }
+            Console.WriteLine($"Сумма чисел кратныx 3 или 5 составляет {summ}");
+        }
+    }
+}
