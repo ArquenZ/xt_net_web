4c959607e617ffdbf3a0737734dc9d6b88feb632
AlexeyZabuga <arquenz@yandex.ru>
2019-11-24 19:43:37 +0400

    Добавил Task1_11

---
+++ Task01/Task1_11/Task1_11 Av_Str_Length.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            Console.WriteLine("Средняя длинна слов в вашей строке = " + AvgLength(Console.ReadLine()));            
+        }
+        static double AvgLength(string str)
+        {
+            if (str.Length == 0) return 0;
+            else
+            {
+                int AllCharsCount = 0;
+                int WordsCount = 0;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    string word = "";
+                    while (i < str.Length && !char.IsSeparator(str[i]) && !char.IsPunctuation(str[i]))
+                    {
+                        word += str[i];
+                        i++;
+                        AllCharsCount++;
+                    }
+                    if (word != "") WordsCount++;
+                }
+                return (double)AllCharsCount / WordsCount;
+
+            }
+            
+        }
+    }
+}
