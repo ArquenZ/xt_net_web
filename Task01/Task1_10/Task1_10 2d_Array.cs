0cb32f7077ef48fd6f9e2043f880e2703348ce90
AlexeyZabuga <arquenz@yandex.ru>
2019-11-24 18:29:27 +0400

    Добавил Task1_10

---
+++ Task01/Task1_10/Task1_10 2d_Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var (arr, sum) = Create2dArr(5,5);
+            foreach (int item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine("Сумма элементов массива с четным индексом = " + sum);
+        }
+        static (int[,]arr,int sum) Create2dArr(int a, int b)
+        {
+            int sum = 0;
+            Random rnd = new Random();
+            int[,] arr = new int[a, b];
+            for (int x = 0; x < a; x++)
+            {
+                for (int y = 0; y < b; y++)
+                {
+                    arr[x, y] = rnd.Next(-100, 100);
+                    if ((x + y) % 2 == 0) sum += arr[x, y];
+                }
+            }
+            return (arr,sum);
+        }
+    }
+}
