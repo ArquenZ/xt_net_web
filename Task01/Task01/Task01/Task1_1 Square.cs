b1971c1b70d82a036884ee3bd12865e618cfe031
AlexeyZabuga <arquenz@yandex.ru>
2019-11-24 11:01:00 +0400

    Добавил Task 1_1 Square

---
+++ Task01/Task01/Task01/Task1_1 Square.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое положительное значение стороны А");
+            int a = InputTest(ReadInput());
+            Console.WriteLine("Введите целое положительное значение стороны B");
+            int b = InputTest(ReadInput());
+            Console.WriteLine("Площадь прямоугольника = " + Square(a, b));
+            //Console.WriteLine(InputTest(ReadInput()));
+        }
+        /// <summary>
+        /// Метод ввода значения с консоли, парсит инт.
+        /// </summary>
+        /// <returns>целое значение</returns>
+        private static int ReadInput()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            return a;
+        }
+        /// <summary>
+        /// Проверка соответствия ввода условиям задания
+        /// </summary>
+        /// <param name="a">Целое значение</param>
+        /// <returns>Значение соответствующее условиям задачи</returns>
+        private static int InputTest(int a)
+        {
+            if (a > 0) return a;
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+                return ReadInput();
+            }
+        }
+        //Во избежания переполнения при умножении двух интов привел к ulong
+        private static ulong Square(int a, int b)
+        {
+            return ((ulong)a * (ulong)b);
+        }
+        
+    }
+}
