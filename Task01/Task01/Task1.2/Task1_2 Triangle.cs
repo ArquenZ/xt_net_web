81c82b2ba53bde2bdda88321e329202a85af7903
AlexeyZabuga <arquenz@yandex.ru>
2019-11-24 11:17:41 +0400

    Добавил Task1_2 Triangle

---
+++ Task01/Task01/Task1.2/Task1_2 Triangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число строк");
+            Triangle(InputTest(ReadInput()));
+            Console.ReadLine();
+        }
+        private static int ReadInput()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            return a;
+        }
+        private static int InputTest(int a)
+        {
+            if (a > 0) return a;
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+                return ReadInput();
+            }
+        }
+        private static void Triangle(int a)
+        {
+            string s ="";
+            for (int i = 0; i < a; i++)                
+            {
+                s = s + "*";                
+                Console.WriteLine(s);
+
+            }
+        }
+    }
+}
