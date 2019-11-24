3cdcb93beeeb3d27c2bdb50dc38df668039cb6b9
AlexeyZabuga <arquenz@yandex.ru>
2019-11-24 12:00:07 +0400

    Добавил Task1_3 Another Triangle

---
+++ Task01/Task01/Task1.3/Task1_3 AnotherTriangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число строк");
+            Triangle(InputTest(ReadInput()));
+            Console.ReadLine();
+        }
+        private static int ReadInput()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            return a;
+        }
+        private static int InputTest(int a)
+        {
+            if (a > 0 && ((a % 2)==1)) return a;
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+                return ReadInput();
+            }
+        }
+        private static void Triangle(int a)
+        {
+            string[] arr = new string[a];
+            string s = "*";
+            //создаем последнюю строку в массиве строк
+            for (int i = 0; i<a-1; i++)
+            {
+                s = s + "**";                
+            }
+            //заполняем оостальной массив
+            for (int i = a-1; i >= 0; i--)
+            {
+                
+                arr[i] = s;
+                s = s.Substring(0, s.Length - 2);
+                s = " " + s;
+            }
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
