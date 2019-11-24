354110eb0ed1f7055b7fd29fb63e548e43cd1f4b
AlexeyZabuga <arquenz@yandex.ru>
2019-11-24 17:48:56 +0400

    Добавил  Task1_8

---
+++ Task01/Task1_8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task1_8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task1_8")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1f668b26-46b4-4bda-9ad2-f317888e3a5c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task01/Task1_8/Task1_8 3dArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] arr = Create3dArr(4, 3, 4);
+            arr = ArrToZero(arr,4,3,4);            
+        }
+
+        static int[,,] Create3dArr(int a, int b, int c)
+        {
+            Random rnd = new Random();
+            int[,,] array = new int[a, b, c];
+            for (int x = 0; x < a; x++)
+            {
+                for (int y = 0; y < b; y++)
+                {
+                    for (int z = 0; z < c; z++)
+                    {
+                        array[x, y, z] = rnd.Next(-100, 100);
+                    }
+                }
+            }
+            return array;
+        }
+        static int[,,] ArrToZero(int[,,] array, int a, int b, int c)
+        {
+            for (int x = 0; x < a; x++)
+            {
+                for (int y = 0; y < b; y++)
+                {
+                    for (int z = 0; z < c; z++)
+                    {
+                        if (array[x, y, z] > 0)
+                        {
+                            array[x, y, z] = 0;
+                        }
+                        Console.Write(" " + array[x, y, z]);                            
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            return array;
+        }
+    }
+}
